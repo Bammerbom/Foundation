3de145fb9ea755e15acc60250cc1684078aee968
Yoram Janssen <yoramjanssenn@gmail.com>
2014-12-14 14:14:13 +0100

    Prettify bootstrap.sh

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
-echo 'BOOTSTRAPPING';
-echo "Foundation by FoundationDriven";
+echo '== FOUNDATION BOOTSTRAP ==';
+echo "This bootstrap script will setup the neccesary project files";
+echo "for your IDE"
 echo 'Eclipse or IDEA? (e or i)';
 read IDE
 case $IDE in
 e)
-	echo "Setting up Eclipse";
+	echo "Setting up project files for Eclipse";
 	./gradlew eclipse
 	;;
 i)
-	echo "Setting up IDEA";
+	echo "Setting up project files for IntelliJ IDEA";
 	./gradlew idea
 esac
