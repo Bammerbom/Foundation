13a329a7a6c1c5ba50196c5dacf288cbb9b718d3
Oscar <oscar.shrimpton.personal@gmail.com>
2014-12-15 17:58:57 +0000

    Added javadocs

--- src/main/java/io/foundationdriven/foundation/api/teleportation/objects/TeleportResult.java
+++ src/main/java/io/foundationdriven/foundation/api/teleportation/objects/TeleportResult.java
@@ -2,30 +2,65 @@ package io.foundationdriven.foundation.api.teleportation.objects;
 
 import org.spongepowered.api.world.Location;
 
+/**
+ * Returned when the teleport function of a waypoint is called, contains information about the teleport as well as whether or not it was succesful
+ */
 public class TeleportResult {
+    /**
+     * The location the entity was at before the teleport
+     */
     private Location from;
+    /**
+     * The waypoint the entity was teleported to
+     */
     private Waypoint to;
+    /**
+     * Whether or not the teleport was successful
+     */
     private boolean successful;
 
-    public Location getFrom() {
-        return from;
-    }
-
-    public Waypoint getTo() {
-        return to;
-    }
-
-    public boolean isSuccessful() {
-        return successful;
-    }
-
+    /**
+     * Sets the variables
+     * @param from the location the entity was at before the teleport
+     * @param to the waypoint the entity was at
+     * @param successful whether or not the teleport was successful
+     */
     public TeleportResult(Location from, Waypoint to, boolean successful) {
 
         this.from = from;
         this.to = to;
         this.successful = successful;
     }
-    public Integer getDistance(){
+
+    /**
+     * Gets from
+     * @return from
+     */
+    public Location getFrom() {
+        return from;
+    }
+
+    /**
+     * Gets to
+     * @return to
+     */
+    public Waypoint getTo() {
+        return to;
+    }
+
+    /**
+     * Gets successful
+     * @return successful
+     */
+    public boolean isSuccessful() {
+        return successful;
+    }
+
+    /**
+     * Gets the distance between the two locations
+     * @return the distance
+     */
+    public Integer getDistance() {
         //TODO
         return 0;
     }
--- src/main/java/io/foundationdriven/foundation/api/teleportation/objects/Waypoint.java
+++ src/main/java/io/foundationdriven/foundation/api/teleportation/objects/Waypoint.java
@@ -7,37 +7,83 @@ import org.spongepowered.api.world.Location;
 
 import java.util.UUID;
 
+/**
+ * A Waypoint is a pretty simple object that represents a place where an entity could be teleported to
+ */
 public class Waypoint {
+    /**
+     * The name of this waypoint
+     */
     private String name;
+    /**
+     * The UUID of this waypoint
+     * @see java.util.UUID
+     */
     private UUID uuid;
+    /**
+     * The location of this waypoint
+     * @see org.spongepowered.api.world.Location
+     */
     private Location loc;
 
+    /**
+     * Gets the name
+     * @return name
+     */
     public String getName() {
 
         return name;
     }
 
+    /**
+     * Sets the variables
+     * @param name the name
+     * @param loc the location
+     */
     public Waypoint(String name, Location loc) {
         this.name = name;
         this.loc = loc;
     }
 
+    /**
+     * Sets the name
+     * @param name the new name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Gets the UUID
+     * @return uuid
+     */
     public UUID getUuid() {
         return uuid;
     }
 
+    /**
+     * Gets the location
+     * @return loc
+     */
     public Location getLoc() {
         return loc;
     }
 
+    /**
+     * Sets the location
+     * @param loc the new loc
+     */
     public void setLoc(Location loc) {
         this.loc = loc;
     }
 
+    /**
+     * Teleports the provided entity to this waypoint
+     * @param e the entity to teleport
+     * @return the TeleportResult
+     * @see io.foundationdriven.foundation.api.teleportation.objects.TeleportResult
+     * @see org.spongepowered.api.entity.Entity
+     */
     public TeleportResult teleport(Entity e) {
         if (loc.getBlock().getType() != BlockTypes.AIR || loc.add(0, 1, 0).getBlock().getType() != BlockTypes.AIR)
             throw new UnsafeTeleport(this);
